e5726c3f097b8218d19a4941b8e4533d31d93af9
Steven Stern <steve@sterndata.com>
2017-03-29 10:38:12 -0500

    added optiosn page

--- functions.php
+++ functions.php
@@ -233,3 +233,14 @@ function vna_get_recent_posts( $count ) {
 		wp_reset_postdata();
 	}
 }
+
+/*
+ * Add ACF Options page
+ */
+acf_add_options_page(array(
+	'page_title'    => 'Theme General Settings',
+	'menu_title'    => 'Theme Settings',
+	'menu_slug'     => 'theme-general-settings',
+	'capability'    => 'edit_posts',
+	'redirect'      => false,
+));
