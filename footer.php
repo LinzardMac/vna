43d81e020dfc48316cef159d3691987d3944ca00
Steven Stern <steve@sterndata.com>
2017-03-29 10:43:27 -0500

    footer color, use ACF fields

--- footer.php
+++ footer.php
@@ -15,9 +15,7 @@
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'vna' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'vna' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'vna' ), 'vna', '<a href="https://automattic.com/" rel="designer">What A Great Website, LLC</a>' ); ?>
+			<?php the_field( 'wagw_footer', 'option' ); ?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
