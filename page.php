34d4dc966547335fe8a910d91a2e7cda91ed0cdf
Steven Stern <steve@sterndata.com>
2017-03-31 11:22:05 -0500

    default featured image support for pages

--- page.php
+++ page.php
@@ -23,6 +23,25 @@ get_header(); ?>
 					?>
 					<div id="featured-image"><?php the_post_thumbnail( 'full' );?></div>
 					<?php
+				} else {
+					// if it has a special body class assigned, use the default image for it.
+					$classes = get_body_class();
+					if (in_array( 'page_about', $classes ) ) {
+						$fi = get_field( 'about_featured_image', 'option' );
+					}
+					if (in_array( 'page_grants', $classes ) ) {
+						$fi = get_field( 'grants_featured_image', 'option' );
+					}
+					if (in_array( 'page_news', $classes ) ) {
+						$fi = get_field( 'news_featured_image', 'option' );
+					}
+					if (isset( $fi ) ) {
+						?>
+						<div id="featured-image">
+							<?php $src_set = ' srcset ="' . wp_get_attachment_image_srcset( $fi['id'] ) . '" '; ?>
+							<img src="<?php echo $fi['url']; ?>" alt="<?php echo $fi['alt']; ?>" <?php echo $src_set; ?> />
+							<?php
+					}
 				}
 				get_template_part( 'template-parts/content', 'page' );
 
